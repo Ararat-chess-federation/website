4117f8a9c3a2dcf798044cbfa9f4c27419892ca9
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-20 14:26:50 +0400

    feat: add Navigation

---
+++ frontend/src/components/header/navigation/Navigation.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { NAVIGATION } from "./constants";
+import "./Navigation.css";
+
+export default function Navigation() {
+  return (
+    <section className="navigation">
+      <nav className="nav">
+        <ul>
+          {NAVIGATION.map((el) => (
+            <li key={el.link} className={`${el.className} nav_li`}>
+              <Link href={el.link}>{el.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </section>
+  );
+}
