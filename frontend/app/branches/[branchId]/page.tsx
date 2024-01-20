c59d81991f18f7abe79b28e0ad7c800ab9e24f38
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-20 23:21:12 +0400

    feat: add default branch code

--- frontend/app/branches/[branchId]/page.tsx
+++ frontend/app/branches/[branchId]/page.tsx
@@ -1,13 +1,27 @@
-const BranchDetail = async ({ params }: any) => {
-  const data = await getData(params.branchId);
-  console.log(data.data[0]);
+import Link from "next/link";
+
+export default async function Branch({ params }: any) {
+  const { data } = await getData(params.branchId);
+  console.log(data[0]);
 
   return (
     <div>
-      <h1>Branch Detail: </h1>
+      <h1>{data[0].attributes.title}</h1>
+      <div>Հասցե՝ {data[0].attributes.address}</div>
+
+      <span>Մարզիչներ՝ </span>
+      <ul className="branches_trainer_list">
+        {data[0].attributes.trainers.data.map((el: any) => (
+          <li key={el.attributes.url} className="branches_trainer">
+            <Link href={`/branches/${el.attributes.url}`}>
+              {el.attributes.fullName}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
 async function getData(url: string) {
   try {
@@ -19,5 +33,3 @@ async function getData(url: string) {
     console.log(e);
   }
 }
-
-export default BranchDetail;
