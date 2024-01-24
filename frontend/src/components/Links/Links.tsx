556eb392382c605c072f498a85d475b9cd396e2b
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-25 01:34:18 +0400

    feat: add Links component

---
+++ frontend/src/components/Links/Links.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { LINKS } from "../../constants/chessLinks";
+import "./Links.css";
+
+export default function Links() {
+  return (
+    <div className="links_container">
+      {LINKS.map((el) => (
+        <ul key={el.url}>
+          <li className="chess_useful_links_li">
+            <a href={el.url} target="_blank">
+              <Image width={50} height={50} alt={el.alt} src={el.img} />
+            </a>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+}
