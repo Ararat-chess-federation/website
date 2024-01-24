00495074805020845cf0bf2e955bfd87c80b4042
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-25 01:34:05 +0400

    feat: add chessLinks constant

---
+++ frontend/src/constants/chessLinks.ts
@@ -0,0 +1,23 @@
+interface ILinks {
+  img: string;
+  alt: string;
+  url: string;
+}
+
+export const LINKS: ILinks[] = [
+  {
+    img: "/lichess.webp",
+    alt: "Lichess",
+    url: "https://lichess.org/team/wDWVDihn",
+  },
+  {
+    img: "/chessFed.webp",
+    alt: "Chess federation",
+    url: "https://www.chessfed.am/",
+  },
+  {
+    img: "/academy.webp",
+    alt: "Chess academy",
+    url: "https://www.chessacademy.am/",
+  },
+];
