1bf950b4bae059425a09066805c445156966d301
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-24 23:35:56 +0400

    feat: add every trainer page

--- frontend/app/trainers/[trainerId]/page.tsx
+++ frontend/app/trainers/[trainerId]/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Markdown from "react-markdown";
+import "../trainers.css";
 
 export default async function Trainer({ params }: any) {
   const { data } = await getData(params.trainerId);
@@ -8,22 +9,31 @@ export default async function Trainer({ params }: any) {
     <div>
       <h1>{data[0].attributes.fullName}</h1>
 
-      <span>Հեռախոսահամար՝ </span>
-      <a href={`tel:${data[0].attributes.phoneNumber}`}>
-        {data[0].attributes.phoneNumber}
-      </a>
+      <p>
+        Հեռախոսահամար՝
+        <a
+          className="phone_number"
+          href={`tel:${data[0].attributes.phoneNumber}`}
+        >
+          {data[0].attributes.phoneNumber}
+        </a>
+      </p>
 
-      <div>Մասնաճյուղեր՝</div>
-      <ul className="trainer_branches_list">
-        {data[0].attributes.branches.data.map((el: any) => (
-          <li key={el.attributes.url} className="trainer_branches">
-            <Link href={`/branches/${el.attributes.url}`}>
-              {el.attributes.address}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <p>Մասնաճյուղեր՝</p>
 
+      <div>
+        <ul className="trainer_branches_list trainer_branches_list_bio">
+          {data[0].attributes.branches.data.map((el: any) => (
+            <li key={el.attributes.url} className="trainer_branches">
+              <Link href={`/branches/${el.attributes.url}`}>
+                {el.attributes.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <h2>Կենսագրություն</h2>
       <Markdown>{data[0].attributes.bio}</Markdown>
     </div>
   );
