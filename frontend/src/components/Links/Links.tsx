51d350490635b56f8694ec924d5b8ca00d4b6473
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-25 02:17:12 +0400

    feat: change div to aside on right side

--- frontend/src/components/Links/Links.tsx
+++ frontend/src/components/Links/Links.tsx
@@ -4,16 +4,22 @@ import "./Links.css";
 
 export default function Links() {
   return (
-    <div className="links_container">
+    <aside className="links_container">
       {LINKS.map((el) => (
         <ul key={el.url}>
           <li className="chess_useful_links_li">
             <a href={el.url} target="_blank">
-              <Image width={50} height={50} alt={el.alt} src={el.img} className="link_img" />
+              <Image
+                width={50}
+                height={50}
+                alt={el.alt}
+                src={el.img}
+                className="link_img"
+              />
             </a>
           </li>
         </ul>
       ))}
-    </div>
+    </aside>
   );
 }
