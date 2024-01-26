86d86bc7131762b5ead2bb6fb6cd13d1f719555d
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-26 22:14:16 +0400

    feat: add DynamicComponent

---
+++ frontend/src/shared/dynamicComponent/DynamicComponent.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Markdown from "react-markdown";
+
+export default function DynamicComponent({
+  el,
+  idx,
+}: {
+  el: any;
+  idx?: number;
+}) {
+  switch (el.__component) {
+    case "text.paragraph":
+      return <Markdown key={idx}>{el.paragraph}</Markdown>;
+
+    case "img.img-with-description":
+      return (
+        <figure key={idx}>
+          <Image
+            width={100}
+            height={100}
+            src={`http://localhost:1337${el.image.data.attributes.url}`}
+            alt={el.description}
+          />
+          <figcaption>{el.description}</figcaption>
+        </figure>
+      );
+
+    default:
+      return null;
+  }
+}
