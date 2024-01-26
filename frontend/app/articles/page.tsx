a7c297b7644df5d7adffa206bf25ce947b09afed
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-26 18:01:14 +0400

    feat: add Articles list styles

--- frontend/app/articles/page.tsx
+++ frontend/app/articles/page.tsx
@@ -1,8 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
 import getData from "../../src/helpers/getData";
+import "./Articles.css";
 
 export default async function Articles() {
   const { data } = await getData("/api/articles?populate=deep");
-  console.log(data);
+  // console.log(data);
 
-  return <h1>Articles</h1>;
+  return (
+    <section className="articles_container">
+      {data.map((el: any) => (
+        <div key={el.attributes.url} className="article_card">
+          <div className="article_photo">
+            <Link
+              href={`/articles/${el.attributes.url}`}
+              key={el.attributes.url}
+            >
+              <Image
+                width={150}
+                height={50}
+                src={`http://localhost:1337${el.attributes.mainImage.data.attributes.url}`}
+                alt={el.attributes.url}
+                className="branch_img"
+                style={{
+                  width: "100%",
+                  height: "auto",
+                  objectFit: "cover",
+                  maxHeight:"300px"
+                }}
+              />
+            </Link>
+          </div>
+
+          <div className="article_info">
+            <Link href={`/articles/${el.attributes.url}`}>
+              <h2 className="article_name">{el.attributes.title}</h2>
+            </Link>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 }
