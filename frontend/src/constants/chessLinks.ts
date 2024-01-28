567afe1f3e5e81a5aa71bb82b93c0b001ab73aa1
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-28 16:23:08 +0400

    fix: fixed chess links as static image

--- frontend/src/constants/chessLinks.ts
+++ frontend/src/constants/chessLinks.ts
@@ -1,27 +1,33 @@
+import lichess from "../../public/lichess.webp";
+import chessFed from "../../public/chessFed.webp";
+import academy from "../../public/academy.webp";
+import facebook from "../../public/facebook.svg";
+import { StaticImageData } from "next/image";
+
 interface ILinks {
-  img: string;
+  img: StaticImageData;
   alt: string;
   url: string;
 }
 
 export const LINKS: ILinks[] = [
   {
-    img: "/lichess.webp",
+    img: lichess,
     alt: "Lichess",
     url: "https://lichess.org/team/wDWVDihn",
   },
   {
-    img: "/chessFed.webp",
+    img: chessFed,
     alt: "Chess federation",
     url: "https://www.chessfed.am/",
   },
   {
-    img: "/academy.webp",
+    img: academy,
     alt: "Chess academy",
     url: "https://www.chessacademy.am/",
-  }, 
+  },
   {
-    img: "/facebook.svg",
+    img: facebook,
     alt: "Facebook",
     url: "https://www.facebook.com/araratchessfed/",
   },
