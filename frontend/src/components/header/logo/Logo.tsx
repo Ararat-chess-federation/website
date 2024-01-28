081e258d07ebf9abfb68ba3795ff6ea8818b1f8d
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-28 16:22:56 +0400

    fix: fixed Logo component as static image

--- frontend/src/components/header/logo/Logo.tsx
+++ frontend/src/components/header/logo/Logo.tsx
@@ -1,19 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import logo from "../../../../public/logo.webp";
 import "./Logo.css";
 
 export default function Logo() {
   return (
     <Link href="/">
       <section className="logo_section">
-        <Image
-          src="/logo.webp"
-          width={50}
-          height={50}
-          alt="logo"
-          className="logo"
-          priority
-        />
+        <Image src={logo} alt="logo" className="logo" priority />
       </section>
     </Link>
   );
