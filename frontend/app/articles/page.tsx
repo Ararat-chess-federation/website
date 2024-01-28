a047eb8b75003c562f107a26dccdc310f35d47f6
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-28 17:42:11 +0400

    feat: minor refactor articles page

--- frontend/app/articles/page.tsx
+++ frontend/app/articles/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import Link from "next/link";
 import getData from "../../src/helpers/getData";
-import getImageSrc from "../../src/helpers/getImageSrc";
 import { IArticle } from "../../src/models/interfaces/article";
 import Img from "../../src/shared/img/Img";
 import "./Articles.css";
@@ -11,26 +9,25 @@ export default async function Articles() {
 
   return (
     <section className="articles_container">
-      {data.map((el) => (
-        <div key={el.attributes.url} className="article_card">
-          <div className="article_photo">
-            <Link href={`/articles/${el.attributes.url}`}>
-              <Img
-                width={500}
-                height={300}
-                src={el.attributes.mainImage}
-                alt={el.attributes.url}
-              />
-            </Link>
-          </div>
+      {data.map(({ attributes }) => {
+        const { url, mainImage, title } = attributes;
 
-          <div className="article_info">
-            <Link href={`/articles/${el.attributes.url}`}>
-              <h2 className="article_name">{el.attributes.title}</h2>
-            </Link>
+        return (
+          <div key={url} className="article_card">
+            <div className="article_photo">
+              <Link href={`/articles/${url}`}>
+                <Img width={500} height={300} src={mainImage} alt={url} />
+              </Link>
+            </div>
+
+            <div className="article_info">
+              <Link href={`/articles/${url}`}>
+                <h2 className="article_name">{title}</h2>
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </section>
   );
 }
