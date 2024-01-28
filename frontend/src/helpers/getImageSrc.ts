f1f9e84b71e41d4d81c18b02ec9fbc368fc50723
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-28 20:19:38 +0400

    feat: add validation for imgUrl

--- frontend/src/helpers/getImageSrc.ts
+++ frontend/src/helpers/getImageSrc.ts
@@ -1,5 +1,5 @@
 import { IImage } from "../models/interfaces/image";
 
 export default function getImageSrc(image: { data: IImage }) {
-  return `${process.env.BACKEND_URL}${image.data.attributes.formats.optimized.url}`;
+  return `${process.env.BACKEND_URL}${image?.data?.attributes?.formats?.optimized?.url}`;
 }
