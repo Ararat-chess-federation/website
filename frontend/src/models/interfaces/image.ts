42d73be5bde01bb22b67e01fcf885ab11534e5e8
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-28 13:09:12 +0400

    feat: optimize Image interface

--- frontend/src/models/interfaces/image.ts
+++ frontend/src/models/interfaces/image.ts
@@ -1,44 +1,30 @@
+interface IImageFormat {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  path: string | null;
+  width: number;
+  height: number;
+  size: number;
+  url: string;
+}
+
+interface IImageAttributes extends IImageFormat {
+  alternativeText: string | null;
+  caption: string | null;
+  formats: {
+    thumbnail: IImageFormat;
+    optimized: IImageFormat;
+  };
+  previewUrl: string | null;
+  provider: string;
+  provider_metadata: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export interface IImage {
   id: number;
-  attributes: {
-    name: string;
-    alternativeText: string | null;
-    caption: string | null;
-    width: number;
-    height: number;
-    formats: {
-      thumbnail: {
-        name: string;
-        hash: string;
-        ext: string;
-        mime: string;
-        path: string | null;
-        width: number;
-        height: number;
-        size: number;
-        url: string;
-      };
-      optimized: {
-        name: string;
-        hash: string;
-        ext: string;
-        mime: string;
-        path: string | null;
-        width: number;
-        height: number;
-        size: number;
-        url: string;
-      };
-    };
-    hash: string;
-    ext: string;
-    mime: string;
-    size: number;
-    url: string;
-    previewUrl: string | null;
-    provider: string;
-    provider_metadata: string | null;
-    createdAt: string;
-    updatedAt: string;
-  };
+  attributes: IImageAttributes;
 }
