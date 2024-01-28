2ea6feb429584254f6c1455038b896196fe95eef
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-28 17:37:07 +0400

    feat: update article component

--- frontend/app/articles/[articleUrl]/page.tsx
+++ frontend/app/articles/[articleUrl]/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import DynamicComponent from "../../../src/shared/dynamicComponent/DynamicComponent";
+import Img from "../../../src/shared/img/Img";
 import getData from "../../../src/helpers/getData";
-import getImageSrc from "../../../src/helpers/getImageSrc";
 import { IArticle } from "../../../src/models/interfaces/article";
 import "./Article.css";
 
@@ -21,12 +20,7 @@ export default async function Article({ params }: IArticleParams) {
     <div>
       <div className="article_header">
         <div className="article_image">
-          <Image
-            width={500}
-            height={200}
-            alt={title}
-            src={getImageSrc(mainImage)}
-          />
+          <Img width={500} height={200} src={mainImage} alt={title} />
         </div>
         <h1 className="article_title">{title}</h1>
       </div>
