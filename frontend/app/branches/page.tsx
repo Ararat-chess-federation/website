1f549f29eb36d54f677950224b709024f693be6d
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-28 17:43:57 +0400

    feat: minor refactor branches

--- frontend/app/branches/page.tsx
+++ frontend/app/branches/page.tsx
@@ -1,39 +1,41 @@
-import Image from "next/image";
 import Link from "next/link";
 import TrainersList from "../../src/shared/trainersList/TrainersList";
 import Address from "../../src/shared/address/Address";
+import Img from "../../src/shared/img/Img";
 import getData from "../../src/helpers/getData";
-import getImageSrc from "../../src/helpers/getImageSrc";
 import { IBranch } from "../../src/models/interfaces/branch";
 import "./branches.css";
-import Img from "../../src/shared/img/Img";
 
 export default async function Branches() {
   const { data }: { data: IBranch[] } = await getData({ type: "branches" });
 
   return (
     <section>
-      {data.map((el) => (
-        <div key={el.attributes.url} className="branch_card">
-          <Link href={`/branches/${el.attributes.url}`}>
-            <Img
-              width={100}
-              height={100}
-              src={el.attributes.mainImage}
-              alt={el.attributes.url}
-              className="branch_img"
-            />
-          </Link>
+      {data.map(({ attributes }) => {
+        const { url, mainImage, title, address } = attributes;
 
-          <div className="branch_info">
-            <Link href={`/branches/${el.attributes.url}`}>
-              <h2 className="branch_name">{el.attributes.title}</h2>
+        return (
+          <div key={url} className="branch_card">
+            <Link href={`/branches/${url}`}>
+              <Img
+                width={100}
+                height={100}
+                src={mainImage}
+                alt={url}
+                className="branch_img"
+              />
             </Link>
-            <Address address={el.attributes.address} />
-            <TrainersList trainers={data[0].attributes.trainers.data} />
+
+            <div className="branch_info">
+              <Link href={`/branches/${url}`}>
+                <h2 className="branch_name">{title}</h2>
+              </Link>
+              <Address address={address} />
+              <TrainersList trainers={data[0].attributes.trainers.data} />
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </section>
   );
 }
