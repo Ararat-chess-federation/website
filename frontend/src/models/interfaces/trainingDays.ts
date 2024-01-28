eb2b84c677038f07a6fb33f4d80404c0c5e9afa1
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-01-28 13:28:07 +0400

    fix: fix trainingDays types

---
+++ frontend/src/models/interfaces/trainingDays.ts
@@ -0,0 +1,18 @@
+import { IArticleText } from "./article";
+import { IImage } from "./image";
+import { ITrainer } from "./trainer";
+
+export interface ITrainingDays {
+  attributes: {
+    url: string;
+    address: string;
+    mapUrl: string | null;
+    title: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    trainers: { data: ITrainer[] };
+    mainImage: { data: IImage };
+    description: IArticleText[];
+  };
+}
--- frontend/src/shared/trainingDays/TrainingDays.tsx
+++ frontend/src/shared/trainingDays/TrainingDays.tsx
@@ -1,23 +1,7 @@
 import Markdown from "react-markdown";
-import { IImage } from "../../models/interfaces/image";
-import { ITrainer } from "../../models/interfaces/trainer";
+import { ITrainingDays } from "../../models/interfaces/trainingDays";
 
-interface IAttributes {
-  attributes: {
-    url: string;
-    address: string;
-    mapUrl: string | null;
-    title: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    trainers: { data: ITrainer[] };
-    mainImage: { data: IImage };
-    description: { id: number; paragraph: string }[];
-  };
-}
-
-export default function TrainingDays({ attributes }: IAttributes) {
+export default function TrainingDays({ attributes }: ITrainingDays) {
   return (
     <div>
       <h3>Պարապմունքի օրեր</h3>
