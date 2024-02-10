1704a3632ff73a86410a990c99a2b9fc52c754d1
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-02-10 14:38:43 +0400

    feat: create helper for Pagination

---
+++ frontend/src/components/pagination/pagination.helpers.ts
@@ -0,0 +1,23 @@
+export const getPages = (totalPages: number, currentPage: number) => {
+  const endNumber = totalPages - 2;
+  const startNumber =
+    currentPage < endNumber - 2 ? currentPage - 1 : endNumber - 3;
+  const isHasDots = totalPages - startNumber > 5;
+
+  return {
+    startNumber: startNumber > 1 ? startNumber : 1,
+    endNumber: endNumber > 0 ? endNumber : 1,
+    isHasDots,
+  };
+};
+
+export const getLengths = (
+  totalPages: number,
+  startNumber: number,
+  endNumber: number
+) => {
+  return {
+    startLength: endNumber - startNumber < 3 ? endNumber - startNumber : 3,
+    endLength: totalPages - endNumber < 2 ? totalPages - endNumber + 1 : 3,
+  };
+};
