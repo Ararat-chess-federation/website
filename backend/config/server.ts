dedc9a6eaa35d3a710b574026fb3bf8d3754d8e6
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-04-22 21:12:35 +0400

    feat: add default value for url in server.ts

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,11 +1,11 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
-  url: (env('SERVER_URL') + '/strapi') || '',
+  url: env("SERVER_URL") ? env("SERVER_URL") + "/strapi" : "",
 });
