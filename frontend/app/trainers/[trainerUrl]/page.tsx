9cad7c3f01e52c1f1d10600882a0519453cd9a95
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-04-22 22:41:49 +0400

    feat: update metadata in Trainer

--- frontend/app/trainers/[trainerUrl]/page.tsx
+++ frontend/app/trainers/[trainerUrl]/page.tsx
@@ -5,15 +5,25 @@ import DataNotFound from "../../../src/shared/dataNotFound/DataNotFound";
 import getData from "../../../src/helpers/getData";
 import { ITrainer } from "../../../src/models/interfaces/trainer";
 import "../trainers.css";
+import { siteTitle } from "../../../src/constants/titles";
 
 interface ITrainerParams {
   params: { trainerUrl: string };
 }
 
-export const metadata = {
-  title: "",
-};
+export async function generateMetadata({ params }: any) {
+  const { data }: { data: ITrainer[] } = await getData({
+    type: "trainers",
+    searchUrl: params.trainerUrl,
+  });
 
+  const { fullName } = data[0].attributes;
+
+  return {
+    title: `${fullName} | ${siteTitle}`,
+    description: `Արարատի մարզի շախմատի ֆեդերացիայի մարզիչ ${fullName}ի կենսագրություն`,
+  };
+}
 export default async function Trainer({ params }: ITrainerParams) {
   const { data }: { data: ITrainer[] } = await getData({
     type: "trainers",
@@ -25,7 +35,6 @@ export default async function Trainer({ params }: ITrainerParams) {
   }
 
   const { fullName, phoneNumber, branches, bio } = data[0].attributes;
-  metadata.title = fullName;
 
   return (
     <div>
