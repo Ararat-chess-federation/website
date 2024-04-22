8b51ff79a37a9ad6ac4d6be0b4bb5aa41e4f64a9
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-04-22 22:43:34 +0400

    feat: add metadata in NotFound

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,3 +1,9 @@
 import NotFound from "../src/components/404/404";
+import { siteTitle } from "../src/constants/titles";
+
+export const metadata = {
+  title: `Էջը չի գտնվել | ${siteTitle}`,
+  description: `Էջը չի գտնվել ${siteTitle}`,
+};
 
 export default NotFound;
