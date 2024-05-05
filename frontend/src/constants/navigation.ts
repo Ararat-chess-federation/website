504cff628a2957d378c4895b490e98e35fec4e9e
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-05-05 11:43:46 +0400

    feat: temporary commented some sections

--- frontend/src/constants/navigation.ts
+++ frontend/src/constants/navigation.ts
@@ -8,18 +8,18 @@ export const NAVIGATION: INavigation[] = [
     link: "/articles",
     title: "Նորություններ",
   },
-  {
-    link: "/trainers",
-    title: "Մարզիչներ",
-  },
+  // {
+  //   link: "/trainers",
+  //   title: "Մարզիչներ",
+  // },
   // {
   //   link: "/statistic",
   //   title: "Վիճակագրություն",
   // },
-  {
-    link: "/branches",
-    title: "Մասնաճյուղեր",
-  },
+  // {
+  //   link: "/branches",
+  //   title: "Մասնաճյուղեր",
+  // },
   // {
   //   link: "/calendar",
   //   title: "Օրացուցային պլան",
