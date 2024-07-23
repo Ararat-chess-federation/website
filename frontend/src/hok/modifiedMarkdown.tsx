3e01d50a0cd2ead691dc10b7556fa2b6084dffc9
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-07-24 02:30:23 +0400

    feat: modify anchor tag

--- frontend/src/hok/modifiedMarkdown.tsx
+++ frontend/src/hok/modifiedMarkdown.tsx
@@ -1,12 +1,32 @@
+import { AnchorHTMLAttributes } from "react";
 import Markdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
 
-type IMarkdownChildren = string | null | undefined;
+interface IMarkdownChildrenProps {
+  children: string | null | undefined;
+}
+type IModifiedAnchor = React.FC<AnchorHTMLAttributes<HTMLAnchorElement>>;
 
-export default function ModifiedMarkdown({ children }: { children: IMarkdownChildren }) {
+export default function ModifiedMarkdown({ children }: IMarkdownChildrenProps) {
+  const LinkRenderer: IModifiedAnchor = ({ href, children }) => {
+    const target = href?.startsWith("/") ? "" : "_blank";
+
+    return (
+      <a href={href} target={target} rel="noopener noreferrer">
+        {children}
+      </a>
+    );
+  };
+
   return (
-    <Markdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>
+    <Markdown
+      rehypePlugins={[rehypeRaw]}
+      remarkPlugins={[remarkGfm]}
+      components={{
+        a: LinkRenderer,
+      }}
+    >
       {children}
     </Markdown>
   );
