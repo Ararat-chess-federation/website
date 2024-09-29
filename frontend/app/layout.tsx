aaebaf60adff54f3459d6a6b4ff4a04de3171b78
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-09-29 17:43:41 +0400

    feat: add analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,6 +5,7 @@ import "../styles/variables.css";
 import "../styles/global.css";
 import "./layout.css";
 import { Metadata } from "next";
+import { CSPostHogProvider } from "./providers";
 
 export const metadata: Metadata = {
   title: "Արարատի մարզի շախմատի ֆեդերացիա",
@@ -18,18 +19,20 @@ interface ILayout {
 export default function RootLayout({ children }: ILayout) {
   return (
     <html lang="hy">
-      <body>
-        <Header />
-        <main className="main_container">
-          <section className="left_menu_container">
-            <LeftMenu />
-          </section>
-          <section className="content_container">{children}</section>
-          <section className="links_container">
-            <Links />
-          </section>
-        </main>
-      </body>
+      <CSPostHogProvider>
+        <body>
+          <Header />
+          <main className="main_container">
+            <section className="left_menu_container">
+              <LeftMenu />
+            </section>
+            <section className="content_container">{children}</section>
+            <section className="links_container">
+              <Links />
+            </section>
+          </main>
+        </body>
+      </CSPostHogProvider>
     </html>
   );
 }
