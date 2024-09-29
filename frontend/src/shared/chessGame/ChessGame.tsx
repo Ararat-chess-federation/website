2a770b58d8911cf6a65ca4ff7eab6b4518213aa9
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-09-29 16:35:30 +0400

    feat: move lichess iframe validation to backend

--- frontend/src/shared/chessGame/ChessGame.tsx
+++ frontend/src/shared/chessGame/ChessGame.tsx
@@ -1,11 +1,10 @@
-import "./ChessGame.css"
+import "./ChessGame.css";
 
 export default function ChessGame({ link }: { link: string }) {
-  const iframeRegex =
-    /^<iframe width="600" height="371" src="https:\/\/lichess\.org\/study\/embed\/[A-Za-z0-9]+\/[A-Za-z0-9]+" frameborder=0><\/iframe>$/;
-  const isValid = iframeRegex.test(link);
-
-  return isValid ? (
-    <div className="chess_game_container" dangerouslySetInnerHTML={{ __html: link }}></div>
-  ) : null;
+  return (
+    <div
+      className="chess_game_container"
+      dangerouslySetInnerHTML={{ __html: link }}
+    ></div>
+  );
 }
