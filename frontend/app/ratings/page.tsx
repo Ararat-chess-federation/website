ffc3e9595dbea7896bbf8ec0ad4e1fedba92d94b
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-10-30 23:20:06 +0400

    feat: add chessfed initial link

--- frontend/app/ratings/page.tsx
+++ frontend/app/ratings/page.tsx
@@ -59,7 +59,15 @@ export default function Ratings() {
           {ratings.slice(1).map((el: string[], idx: number) => (
             <tr key={idx}>
               {el.map((nextEl, idx) => (
-                <td key={idx}>{nextEl}</td>
+                <td key={idx}>
+                  {nextEl.includes("/am/profile") ? (
+                    <a href={`https://chessfed.am${nextEl}`} target="_blank">
+                      Պռոֆիլ
+                    </a>
+                  ) : (
+                    nextEl
+                  )}
+                </td>
               ))}
             </tr>
           ))}
