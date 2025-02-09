4d89e664c6c0abccdd3bf860f9a4b13cd9538db7
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2025-02-09 20:34:58 +0400

    feat: update images

--- frontend/src/helpers/getImageSrc.ts
+++
@@ -1,5 +0,0 @@
-import { IImage } from "../models/interfaces/image";
-
-export default function getImageSrc(image: { data: IImage }) {
-  return `${process.env.NEXT_PUBLIC_BACKEND_URL}${image?.data?.attributes?.formats?.optimized?.url}`;
-}
