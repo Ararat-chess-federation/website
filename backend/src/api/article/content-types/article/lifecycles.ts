4b425fa5260bf9bebacb0c3c95148595c7bb5eac
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2025-12-14 01:09:22 +0400

    feat: add date suffix for url

--- backend/src/api/article/content-types/article/lifecycles.ts
+++ backend/src/api/article/content-types/article/lifecycles.ts
@@ -1,6 +1,24 @@
 export default {
   async beforeCreate(event) {
-    const { publishDate } = event.params.data;
+    const { publishDate, url } = event.params.data;
     event.params.data.publishDate = publishDate ? publishDate : new Date();
+    event.params.data.url = appendDateToString(url ?? "");
   },
+  async beforeUpdate(event) {
+    const { url } = event.params.data;
+    event.params.data.url = appendDateToString(url ?? "");
+  }
 };
+
+function appendDateToString(url: string) {
+  const now = new Date();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const year = String(now.getFullYear()).slice(-2);
+  const datePattern = /-\d{2}-\d{2}$/;
+
+  if (datePattern.test(url)) {
+    return url;
+  }
+
+  return `${url}-${month}-${year}`;
+}
