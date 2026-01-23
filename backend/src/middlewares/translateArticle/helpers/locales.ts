5348e8d795c4d73b9915747d09aa43cca26ddb3b
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2026-01-23 21:52:44 +0400

    feat: add ts

--- backend/src/middlewares/translateArticle/helpers/locales.ts
+++ backend/src/middlewares/translateArticle/helpers/locales.ts
@@ -1,13 +1,14 @@
 import { DEFAULT_LOCALE_CODE } from "../constants/locale";
+type tLocaleCode = "hy" | "ru" | "en";
 
 export async function getLocalesList() {
-    const localesData = await strapi.plugins.i18n.services.locales.find();
+    const localesData: { code: tLocaleCode }[] = await strapi.plugins.i18n.services.locales.find();
 
     return localesData
         .map((locale) => locale.code)
         .filter((localeCode) => localeCode !== DEFAULT_LOCALE_CODE)
 }
 
-export function getExistingLocales(localizations) {
+export function getExistingLocales(localizations: { locale: tLocaleCode }[]) {
     return localizations.map((el) => el.locale);
 }
