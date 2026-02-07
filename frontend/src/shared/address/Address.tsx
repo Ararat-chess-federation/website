274a1535f582a851571cacbf459b8acafa255cd1
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2026-02-08 01:51:39 +0400

    feat: add translations for branches page

--- frontend/src/shared/address/Address.tsx
+++ frontend/src/shared/address/Address.tsx
@@ -1,9 +1,13 @@
+"use client"
+
+import { useTranslations } from "next-intl";
 import styles from "./address.module.scss";
 
 export default function Address({ address }: { address: string }) {
+  const t = useTranslations()
   return (
     <p className={styles.address}>
-      <span className={styles.address_title}>Հասցե`</span>
+      <span className={styles.address_title}>{t("contactsInfo.address")}</span>
       <span className={styles.main_address}>{address}</span>
     </p>
   );
