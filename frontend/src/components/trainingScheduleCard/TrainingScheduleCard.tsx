cbc18f7310e2bb2c056669f896341321f4dae5ed
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2026-02-09 05:09:46 +0400

    fix: trainingDays translation

--- frontend/src/components/trainingScheduleCard/TrainingScheduleCard.tsx
+++ frontend/src/components/trainingScheduleCard/TrainingScheduleCard.tsx
@@ -1,8 +1,11 @@
+"use client"
+
 import React from "react";
 import styles from "./trainingScheduleCard.module.scss";
 import { IArticleText } from "../../models/interfaces/article";
 import { ITrainer } from "../../models/interfaces/trainer";
 import ModifiedMarkdown from "../../hok/modifiedMarkdown";
+import { useTranslations } from "next-intl";
 
 interface TrainingScheduleCardProps {
   trainerData: ITrainer[];
@@ -13,13 +16,15 @@ export default function TrainingScheduleCard({
   trainerData,
   scheduleData,
 }: TrainingScheduleCardProps) {
+  const t = useTranslations("branchesInfo");
+
   const trainingData = trainerData.map((trainer, index) => ({
     trainerName: trainer.fullName,
     scheduleTexts: scheduleData[index].paragraph,
   }));
   return (
     <div className={styles.training_card}>
-      <h3 className={styles.training_title}>Պարապմունքի օրեր</h3>
+      <h3 className={styles.training_title}>{t("trainingDays")}</h3>
       {trainingData.map((el) => (
         <div key={el.trainerName} className={styles.trainer_schedule}>
           <p className={styles.trainer_name}>{el.trainerName}</p>
