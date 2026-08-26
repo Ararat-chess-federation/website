f0979faac18889ec95ef07b46dc3c9f68ca654e2
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2026-08-26 17:10:35 +0400

    fix: build

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,16 +1,18 @@
 import NotFound from "../src/components/404/404";
+import { NextIntlClientProvider } from "next-intl";
+import { messagesMap } from "../i18n/messages";
 import generatePageMetadata from "../src/helpers/generatePageMetadata";
-import { IPageProps } from "../src/models/interfaces/params";
 
 const DEFAULT_LOCALE = "hy" as const;
 
-export async function generateMetadata(props: IPageProps) {
-  const params = await props.params;
-  const locale = params?.locale ?? DEFAULT_LOCALE;
-
-  return generatePageMetadata({ type: "notFound", locale });
+export function generateMetadata() {
+  return generatePageMetadata({ type: "notFound", locale: DEFAULT_LOCALE });
 }
 
 export default function NotFoundPage() {
-  return <NotFound />
+  return (
+    <NextIntlClientProvider locale={DEFAULT_LOCALE} messages={messagesMap[DEFAULT_LOCALE]}>
+      <NotFound />
+    </NextIntlClientProvider>
+  );
 }
