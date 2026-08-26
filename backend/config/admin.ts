79da7e5c197889edc827f6760d457d5ed21b5062
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2026-08-26 22:24:52 +0400

    feat: add cookie config

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -1,6 +1,9 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    cookie: {
+      path: env('SERVER_URL') ? '/strapi/admin' : '/admin',
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
